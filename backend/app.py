ecbb77addbba671c0aa4ff43d9c2afeacfe87ae5
magorbalassy <magor.balassy@gmail.com>
2023-04-30 09:52:14 +0200

    Added scheduling logic.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import json, logging, os, requests, sys, time
 from datetime import datetime
-from flask import Flask, jsonify, render_template, request
+from flask import current_app, Flask, jsonify, render_template, request
 from peewee import *
 from threading import Timer
 
@@ -9,8 +9,6 @@ logging.basicConfig(filename="/dev/stdout", level=logging.DEBUG,
                     datefmt='%m/%d/%Y %I:%M:%S %p')
 logging.info('==== Starting new run ====')
 
-# Job threads array
-job_array = []
 
 app = Flask(__name__)
 
@@ -52,13 +50,12 @@ if not os.access(CONFIG_DIR, os.W_OK):
 
 app.config.update(dict(
     DATABASE=os.path.join(CONFIG_DIR, 'jobs.db'),
-    DEBUG=True,
-    SECRET_KEY='development key'))
+    DEBUG=True))
+app.config['db'] = SqliteDatabase(app.config['DATABASE'])
+app.config['jobs'] = []
+app.config['job_timers'] = {}
 
-db = SqliteDatabase(app.config['DATABASE'])
-
-
-def custom_response(msg):
+def json_response(msg):
   response = jsonify(msg)
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', '*')
@@ -68,12 +65,46 @@ def custom_response(msg):
 # Create database file and table(s)
 # Run once
 def create_tables():
-    with db:
+    with app.config['db'] as db:
         db.create_tables([Jobs])
-        add_job()
 
 def load_jobs():
-    pass 
+    jobs = [ j for j in Jobs.select() ]
+    json_jobs = []
+    for j in jobs:        
+        json_jobs.append({
+            "filename": j.filename,
+            "length": j.length,
+            "url": j.url,
+            "status": j.status,
+            "start_date": j.start_date,
+            "created_on": j.created_on,
+            "id": j.id            
+        })
+        with app.app_context():
+            scheduler(j)
+            logging.debug(f'{app.config}')
+    return json_jobs
+
+def get_ts():
+    return int(datetime.now().strftime('%s'))*1000
+
+def scheduler(j):
+    if j.id not in app.config['job_timers']:
+        now = get_ts()
+        if now > j.start_date:
+            logging.info(f'Job with ID {j.id} has a start date in the past, can\'t be scheduled.')
+            return False
+        else:
+            start_in_seconds = (j.start_date - now)/1000
+            logging.info(f'Time until schedule: {start_in_seconds} seconds.')
+            t = Timer(start_in_seconds, save_stream, [j.length * 60, j.filename, j.url])
+            t.start()
+            with app.app_context():
+                app.config['job_timers'][j.id] = "t"
+            logging.info(f'Scheduled job with id {j.id}.')
+    else:
+        logging.debug(f'Job with {j.id} id is already scheduled.')
 
 # The SQLite model
 class BaseModel(Model):
@@ -85,15 +116,16 @@ class Jobs(BaseModel):
     length = IntegerField()
     url = TextField()
     status = TextField()
-    start_date = DateTimeField(default=0)
+    start_date = IntegerField()
     created_on = DateTimeField(default=datetime.now)
 
-def save_stream2(duration, name, url):
+# Save to MP4
+def save_stream(duration, name, url):
     time_elapsed = 0
     start_time_in_seconds = time.time()
     if '<datetime>' in name:
         name.replace('<datetime>', time.strftime("%Y%m%d%H%M%S",time.localtime()))
-    with open(f'{name}.mp4', 'wb') as f_out:
+    with open(f'{DATA_DIR}/{name}.mp4', 'wb') as f_out:
         r = requests.get(url, headers=headers, stream=True)
         logging.info(f'Starting recording {name}, http response from stream is: ', r)
         for chunk in r.iter_content(chunk_size=1024):
@@ -102,25 +134,6 @@ def save_stream2(duration, name, url):
             if chunk:
                 f_out.write(chunk)
             time_elapsed = int(time.time() - start_time_in_seconds)
-
-def save_stream(length, name, url):
-    logging.info("Recording video...")
-    filename = time.strftime("/tmp/" + "%Y%m%d%H%M%S",time.localtime())+".avi"
-    file_handle = open(filename, 'wb')
-    chunk_size = 1024
-
-    start_time_in_seconds = time.time()
-
-    time_limit = 10 # time in seconds, for recording
-    time_elapsed = 0
-    with requests.Session() as session:
-        response = session.get(url, stream=True)
-        for chunk in response.iter_content(chunk_size=chunk_size):
-            if time_elapsed > time_limit:
-                break
-            if chunk:
-                file_handle.write(chunk)
-    file_handle.close()
 
 # 404 error for non-existing URLs.
 @app.errorhandler(404)
@@ -132,75 +145,66 @@ def page_not_found(e):
 
 @app.route('/', methods=['OPTIONS'])
 def return_headers():
-    return custom_response('')
+    return json_response('')
 
 @app.route('/', methods=['GET'])
 def home():
-    jobs = [ j for j in Jobs.select() ]
-    job_list = []
-    for j in jobs:        
-        job_list.append({
-            "filename": j.filename,
-            "length": j.length,
-            "url": j.url,
-            "status": j.status,
-            "start_date": j.start_date,
-            "created_on": j.created_on,
-            "id": j.id            
-        })
-    logging.info('Scheduler: ',scheduler.queue)
-    return custom_response(job_list)
-
+    return json_response(load_jobs())
 
 @app.route('/', methods=['POST'])
 def add_job():
     job = request.json
-    jobs = [ j for j in Jobs.select() ]
+    jobs = [ _ for _ in Jobs.select() ]
     for j in jobs:
         if job['filename'] == j.filename and\
            job['length'] == j.length and\
            job['url'] == j.url and\
-           datetime.utcfromtimestamp(int(job['start_date'])/1000) == j.start_date :
-            return custom_response('0')
-    with db.atomic():
+           int(job['start_date']) == j.start_date :
+            return json_response('0')
+    with app.config['db'].atomic():
         j = Jobs.create(
             filename = job['filename'],
             length = job['length'],
             status = 'new',
             url = job['url'],
-            start_date = datetime.utcfromtimestamp(int(job['start_date'])/1000)
+            start_date = int(job['start_date'])
         )
         j.save()
-        return custom_response(j.id);   
+        with app.app_context():
+            scheduler(j)
+        return json_response(j.id);   
 
 @app.route('/', methods=['DELETE'])
 def del_job():
-    print(request.json)
     qry = Jobs.delete().where(Jobs.id == request.json['id'])
     try:
         qry.execute()
-        logging.debug(f'Deleted job {request.json}.')
-        return custom_response(f"{request.json['id']}")
     except:
-        return custom_response(0)
+        logging.info(f'An error occurred while removing job {request.json}.')
+        return json_response(0)
+    logging.info(f'Deleted job {request.json}.')
+    if request.json['id'] in app.config['job_timers']:
+        app.config['job_timers'][request.json['id']].cancel()
+        logging.info(f'Cancelled scheduled job {request.json}.')
+    return json_response(f"{request.json['id']}")
 
 @app.route('/check_db')
 def check_db():
     db_file = app.config['DATABASE']
     if not os.path.isfile(db_file):
-        logging.info(f'Creating database {db_file} and the jobs table.')
+        logging.info(f'Database not found. Creating database {db_file} and the jobs table.')
         try:
             create_tables()
         except:
             logging.error(f'Could not create database at {db_file}.')
             logging.error(f'Call endpoint /check_db to retry recreating the database.')
-            return f'Failed to create database at {db_file}.'
+            return f'Failed to create database at {db_file}. Verify that folder is writable.'
         return f'Created database {db_file}.'
     else:
         logging.info(f'Found database at {db_file}.')
         return f'Using database at {db_file}.'
 
-
+check_db()
+load_jobs()
 if __name__ == "__main__":
-    check_db()
     app.run(debug=True, host='0.0.0.0', port='5000')
